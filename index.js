0947c864bd1e4d8d89dd2f44fc8ffd7985c86b3f
ylanwb <class-computer@class-computers-MacBook-Air.local>
2022-03-09 18:31:40 -0800

    finished excersize 1 through 5

--- index.js
+++ index.js
@@ -1,82 +1,132 @@
-/*
-Exercise 1 
+// Exercise 1
 
-Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check 
-if the current number is odd or even, and display a message to the screen.
+// Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check
+// if the current number is odd or even, and display a message to the screen.
 
-Sample Output : 
-"0 is even" 
-"1 is odd" 
-"2 is even" 
----------- 
----------- 
+// Sample Output :
+// "0 is even"
+// "1 is odd"
+// "2 is even"
+// ----------
+// ----------
 
-Exercise 2
+for (let a = 0; a <= 15; a++) {
+  if (a % 2 === 0) {
+    console.log(`${a} is even`);
+  } else {
+    console.log(`${a} is odd`);
+  }
+}
 
-7. Write a JavaScript program which iterates the integers from 1 to 100. 
-But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". 
-For numbers which are multiples of both three and five print "FizzBuzz". Go to the editor 
-0 1 2 "Fizz" 4 "Buzz" "Fizz" 7 8 "Fizz" "Buzz" etc
+// Exercise 2
 
-Exercise 3
+// 7. Write a JavaScript program which iterates the integers from 1 to 100.
+// But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz".
+// For numbers which are multiples of both three and five print "FizzBuzz". Go to the editor
+// 0 1 2 "Fizz" 4 "Buzz" "Fizz" 7 8 "Fizz" "Buzz" etc
 
-Get the sum of two arrays…actually the sum of all their elements.
-P.S. Each array includes only integer numbers. Output is a number too.
+for (inputLoop = 1; inputLoop <= 100; inputLoop++) {
+  if (inputLoop % 3 === 0 && inputLoop % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (inputLoop % 3 === 0) {
+    console.log("Fizz");
+  } else if (inputLoop % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(inputLoop);
+  }
+}
 
-let arr_1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; // --> 276
-let arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; // --> 351
-Example output: 
-276 + 351 = 627
+// Exercise 3
 
-Exercise 4 
+// Get the sum of two arrays… actually the sum of all their elements.
+// P.S. Each array includes only integer numbers. Output is a number too.
 
-Using a for loop print all even numbers up to and including n. Don’t include 0.
+// let arr_1 = [3, 5, 22, 5,  7,  2,  45, 75, 89, 21, 2]; // --> 276
+// let arr_2 = [9, 2, 42, 55, 71, 22, 4,  5,  90, 25, 26]; // --> 351
+// Example output:
+// 276 + 351 = 627
 
-let n1 = 22; 
-Example output: 
-2 4 6 8 10 12 14 16 18 20 22 OR each item on a new line
+let arr_1 = [3, 5, 22, 5, 7, 2, 45, 75, 89, 21, 2];
+let arr_2 = [9, 2, 42, 55, 71, 22, 4, 5, 90, 25, 26];
+let answer = 0;
+for (let i = 0; i < arr_1.length; i++) {
+  answer += arr_1[i];
+}
+for (let i = 0; i < arr_2.length; i++) {
+  answer += arr_2[i];
+}
 
-Exercise 5 
+console.log(`${answer}`);
 
-Given a string change the every second letter to an uppercase ‘Z’. Assume
-there are no space.
+// Exercise 4
 
-let str1 = "javascript";  
-Example output: 
-jZvZsZrZpZ OR each letter on a new line
-HINT: You can use  if((i+1) % 2 == 0) to check for even indexes 
+// Using a for loop print all even numbers up to and including n. Don’t include 0.
 
-Exercise 6
+// let n1 = 22;
+// Example output:
+// 2 4 6 8 10 12 14 16 18 20 22 OR each item on a new line
 
-Check if a string contains the letter “y”. Print “yes” if it does and “no” if it does not.
+let n1 = 22;
 
-let str2 = "don’t know why";
-Example output: 
-“yes”
+for (let number = 1; number <= n1; number++) {
+  if (number % 2 === 0) {
+    console.log(number);
+  }
+}
 
-Exercise 7
+// Exercise 5
 
-Given a number n Calculate the factorial of the number
+// Given a string change the every second letter to an uppercase ‘Z’. Assume
+// there are no space.
 
-let n2 = 4; //  4 * 3 * 2 * 1 = 24
-Example output:
-24
+// let str1 = "javascript";
+// Example output:
+// jZvZsZrZpZ OR each letter on a new line
+// HINT: You can use  if((i+1) % 2 == 0) to check for even indexes
 
- Exercise 8
+let str1 = "javascript";
+let result;
+let replacement = "z";
+//1. separated characters in the string
+//2. divided them by 2
 
-Multiplication Tables
-Write a for loop that will iterate from 0 to 10. For each iteration of the
-for loop, it will multiply the number by 9 and log the result
-(e.g. "2 * 9 = 18").
-Bonus: Use a nested for loop to show the tables for every multiplier from
-1 to 10 (100 results total).
+for (let i = 0; i < str1.length; i++) {
+  if ((i + 1) % 2 === 0) {
+    str1 = str1.substring(0, i) + replacement + str1.substring(i + 1);
+  }
+}
 
-Exercise 9 
+console.log(str1);
+// Exercise 6
 
-The Grade Assigner
-Check the results of your assignGrade function from the conditionals exercise
-by logging every value from 60 to 100: your log should show "For 88, you got a
-B. For 89, you got a B. For 90, you got an A. For 91, you got an A.", etc.,
-logging each grade point in the range.
+// Check if a string contains the letter “y”. Print “yes” if it does and “no” if it does not.
 
-*/
+// let str2 = "don’t know why";
+// Example output:
+// “yes”
+
+// Exercise 7
+
+// Given a number n Calculate the factorial of the number
+
+// let n2 = 4; //  4 * 3 * 2 * 1 = 24
+// Example output:
+// 24
+
+//  Exercise 8
+
+// Multiplication Tables
+// Write a for loop that will iterate from 0 to 10. For each iteration of the
+// for loop, it will multiply the number by 9 and log the result
+// (e.g. "2 * 9 = 18").
+// Bonus: Use a nested for loop to show the tables for every multiplier from
+// 1 to 10 (100 results total).
+
+// Exercise 9
+
+// The Grade Assigner
+// Check the results of your assignGrade function from the conditionals exercise
+// by logging every value from 60 to 100: your log should show "For 88, you got a
+// B. For 89, you got a B. For 90, you got an A. For 91, you got an A.", etc.,
+// logging each grade point in the range.
